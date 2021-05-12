779728559c5e657fc7fbb3ef2445a841fb7c4d85
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-05-12 10:37:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
        return string.replace(/[^A-Za-z0-9-' ]+/g, '')
     }
 
+<<<<<<< HEAD
 static  titleize(sentence){
   let unnecessary=["the","a","an","but","of","and","for","at","by","from"];
   let words=sentence.split(" ");
@@ -21,10 +22,28 @@ static  titleize(sentence){
         }
     }
         if (counter===10)
+=======
+static titleize(sentence){
+  let unnecessary=["a","an","but","of","and","for","at","by","from"];
+  let words=sentence.split(" ");
+  let counter=0;
+    for(let i=0;i<words.length;i++){
+    for(let j=0;j<unnecessary.length;j++){
+        if(words[i]!=unnecessary[j])
+        {
+            counter++;
+        }
+    }
+        if (counter===9)
+>>>>>>> 13f353b788818cae721449354ae2c87d5d9352e8
         {words[i]=words[i].charAt(0).toUpperCase()+words[i].slice(1);}
 
 counter=0;
 }
 return words.join(" ");
 }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 13f353b788818cae721449354ae2c87d5d9352e8
